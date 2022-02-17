69464da95b27bfaff309488583c58d0fa4a1ada5
Aska-Hou <yueranh@kean,edu>
2022-02-17 16:19:36 +0800

    Modified2

--- AOPTest/src/main/java/org/example/service/TransferMoneyService.java
+++ AOPTest/src/main/java/org/example/service/TransferMoneyService.java
@@ -5,6 +5,6 @@ import org.example.pojo.Account;
 
 public interface TransferMoneyService {
 
-    // Modified1
+    // Modified3
     public boolean transferMoney(Account fromAccount, Account targetAccount, double money) throws Exception;
 }
