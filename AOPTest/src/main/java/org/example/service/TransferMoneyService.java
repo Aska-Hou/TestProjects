a7e065a7290aaefe2f1127de2e5cb2464011d8c6
Aska-Hou <yueranh@kean,edu>
2022-02-18 11:39:29 +0800

    Transfer - DEV Modify

--- AOPTest/src/main/java/org/example/service/TransferMoneyService.java
+++ AOPTest/src/main/java/org/example/service/TransferMoneyService.java
@@ -5,6 +5,6 @@ import org.example.pojo.Account;
 
 public interface TransferMoneyService {
 
-    // Modified3
+    // Modified dev
     public boolean transferMoney(Account fromAccount, Account targetAccount, double money) throws Exception;
 }
